362fa417db40c7e621be6c3f24c202f35d98d19d
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2024-09-06 09:20:48 -0300

    Mais ajustes iniciais para o pacote

--- R/smEn.R
+++ R/smEn.R
@@ -1,3 +1,13 @@
+#' Simulador de Efeitos de Ambiente não identificável
+#'
+#' @param nind número de indivíduos da população
+#' @param m média da distribuição normal
+#' @param dp desvio-padrão da distribuição normal
+#'
+#' @return um vetor com os efeito de ambiente simulados
+#' @export
+#'
+#' @examples smEn(nind=500,m=0,dp=25)
 smEn<-function(nind,m,dp){
 
   temp<-stats::rnorm(nind,m,dp)
